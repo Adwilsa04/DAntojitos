28aa521c12c6b0299a7f64b1d11d79a848b20377
WanChizu <lizortega747@gmail.com>
2024-05-07 09:52:36 -0400

    formularios registro, inicio y pago

---
+++ resources/views/Formularios/pago.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <link rel="stylesheet" href="css/pago.css">
+
+</head>
+<body>
+
+<div class="container">
+
+    <form action="">
+
+        <div class="row">
+
+            <div class="col">
+
+                <h3 class="title">Datos Personales</h3>
+
+                <div class="inputBox">
+                    <span>full name :</span>
+                    <input type="text" placeholder="john deo">
+                </div>
+                <div class="inputBox">
+                    <span>email :</span>
+                    <input type="email" placeholder="example@example.com">
+                </div>
+                <div class="inputBox">
+                    <span>address :</span>
+                    <input type="text" placeholder="room - street - locality">
+                </div>
+                <div class="inputBox">
+                    <span>city :</span>
+                    <input type="text" placeholder="mumbai">
+                </div>
+
+                <div class="flex">
+                    <div class="inputBox">
+                        <span>state :</span>
+                        <input type="text" placeholder="india">
+                    </div>
+                    <div class="inputBox">
+                        <span>zip code :</span>
+                        <input type="text" placeholder="123 456">
+                    </div>
+                </div>
+
+            </div>
+
+            <div class="col">
+
+                <h3 class="title">Cuenta de Banco</h3>
+                <div class="inputBox">
+                    <span>Nombre del banco:</span>
+                    <input type="text" placeholder="Santa Cruz">
+                </div>
+                <div class="inputBox">
+                    <span>Cuenta de Banco:</span>
+                    <input type="number" placeholder="1111-2222-3333-4444">
+                </div>
+                <div class="inputBox">
+                    <span>Nombre de la cuenta de Banco:</span>
+                    <input type="text" placeholder="january">
+                </div>
+               
+    
+        </div>
+        </div>
+        <input type="submit" value="proceed to checkout" class="submit-btn">
+
+    </form>
+</div>
+</div>    
+    
+</body>
+</html>
