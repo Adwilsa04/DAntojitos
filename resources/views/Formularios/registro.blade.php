9e110cd0aa9f5e7c1bb14f7632086c1e1812f1f7
WanChizu <lizortega747@gmail.com>
2024-05-13 13:44:59 -0400

    a

--- resources/views/Formularios/registro.blade.php
+++ resources/views/Formularios/registro.blade.php
@@ -82,7 +82,7 @@
             </div>
           </div>
           <div class="footer-link padding-top--24">
-            <span>¿Estás registrado? Inicia sesión <a href="{{route('inicio')}}">Iniciar Sesión</a></span>
+            <span>¿Estás registrado? Inicia sesión <a href="{{route('iniciogit')}}">Iniciar Sesión</a></span>
             <div class="listing padding-top--24 padding-bottom--24 flex-flex center-center">
             <span><a href="#">© D' Antojitos Gourmet Decora</a></span>
             <span><a href="#">Volver a Inicio</a></span>
