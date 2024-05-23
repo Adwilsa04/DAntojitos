6a0838d1f68fd71a91189a78059e59a7f1f94a9a
adwilsa <adwilsa@gmail.com>
2024-05-23 07:54:32 -0400

    ESTOY JARTA DE LOS ERRORES

--- resources/views/pedidos/index.blade.php
+++ resources/views/pedidos/index.blade.php
@@ -30,9 +30,10 @@
 
             <div class="item">
             <img src="{{ asset('images/2.png') }}" alt="Descripción de la imagen">
-                <h2>CoPilot / Black / Automatic</h2>
-                <div class="price">$550</div>
-                <button href="checkout.html">Add To Cart</button>
+            <center>
+                <h2>Menu 1</h2>
+                <div class="price">$10,000.00</div></center>
+                <button href="checkout.html">Añadir</button>
             </div>
 
         </div>
